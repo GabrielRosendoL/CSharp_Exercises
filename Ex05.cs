ae01b350396f6592f33939b8b5ba88e1cd89943c
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-03-24 11:37:09 -0300

    Update Ex05

--- Ex05.cs
+++ Ex05.cs
@@ -1,19 +1,40 @@
 // Exercício 5: Calculando o fatorial de um número
 
+// Definição de uma classe chamada 'Ex05'.
+// Uma classe é um contêiner que pode conter métodos, propriedades e outros membros.
+// Classes são fundamentais na programação orientada a objetos.
 class Ex05
 {
-
+    // Definição de um método estático chamado 'Run'.
+    // 'public' significa que o método pode ser acessado de fora da classe.
+    // 'static' significa que o método pertence à classe e não a uma instância específica da classe.
+    // Isso significa que você pode chamar o método 'Run' diretamente usando o nome da classe 'Ex05',
+    // sem precisar criar um objeto dessa classe. Métodos estáticos são úteis para funcionalidades
+    // que não dependem de dados específicos de uma instância da classe.
+    // 'void' significa que o método não retorna nenhum valor.
     public static void Run()
     {
-
+        // Declaração de uma variável inteira 'num' que armazena o número para o qual queremos calcular o fatorial.
+        // Neste exemplo, estamos calculando o fatorial de 5.
         int num = 5;
+
+        // Declaração de uma variável inteira 'factorial' inicializada com 1.
+        // Essa variável será usada para armazenar o resultado do cálculo do fatorial.
+        // Ela começa com 1 porque a multiplicação por 1 não altera o resultado.
         int factorial = 1;
 
+        // Um loop 'for' que itera de 1 até o valor de 'num' (inclusive).
+        // O loop é usado para calcular o fatorial multiplicando os números de 1 até 'num'.
         for (int i = 1; i <= num; i++)
         {
+            // Multiplica o valor atual de 'factorial' pelo valor de 'i'.
+            // O resultado é armazenado novamente na variável 'factorial'.
+            // Isso significa que 'factorial *= i' é o mesmo que 'factorial = factorial * i'.
             factorial *= i;
         }
 
+        // O método 'Console.WriteLine' é usado para imprimir uma mensagem no console.
+        // Aqui, ele imprime o resultado do cálculo do fatorial.
         Console.WriteLine("O fatorial de " + num + " é " + factorial);
     }
 }
@@ -33,3 +54,6 @@ class Ex05
 // NOTA V: Em cada iteração, o valor de 'i' é multiplicado pelo valor atual de 'factorial' e 
 // o resultado é armazenado em 'factorial'.
 // Ou seja, escrever [factorial *= i] é o mesmo que escrever [factorial = factorial * i].
+
+// NOTA VI: O método 'Console.WriteLine' imprime o resultado do cálculo no console.
+// Neste caso, ele exibe a mensagem "O fatorial de 5 é 120".
--- obj/Debug/net9.0/CSharp_Exercises.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSharp_Exercises.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp_Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742e5e81bc9c7ba1b8e775c4a3f1224f8aee1e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83aa4fe5068261ebf726d81fe774d2a5790b8e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp_Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp_Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
