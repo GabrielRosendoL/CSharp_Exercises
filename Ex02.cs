1c9ebbb9e9999081ee79f90454bfca4d270b2179
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-04-11 00:17:09 -0300

    Update Ex03 New

---
+++ Ex02.cs
@@ -0,0 +1,29 @@
+// Exercício 02: Calculando a média de três números
+// 
+// Objetivo: Completar o código abaixo para calcular e exibir a média de três números.
+// 
+// Instruções:
+// 1. Declare três variáveis inteiras e atribua valores a elas
+// 2. Calcule a média desses números (soma dividida por 3)
+// 3. Exiba o resultado no console usando Console.WriteLine()
+// 4. A mensagem deve mostrar os números e o resultado da média
+// 
+// Dica: Use o operador + para somar os números e / para dividir
+// Dica: Como estamos usando números inteiros, o resultado da divisão será um número inteiro
+
+class Ex02
+{
+    public static void Run()
+    {
+        // TODO: Declare três variáveis e atribua valores a elas
+        // Sugestão: use números como 10, 20 e 30
+        
+        // TODO: Calcule a média dos números
+        // Lembre-se: média = (num1 + num2 + num3) / 3
+        
+        // TODO: Exiba o resultado no console
+        // A mensagem deve ser no formato: "A média entre X, Y e Z é W"
+        // onde X, Y e Z são os números e W é o resultado da média
+        
+    }
+} 
