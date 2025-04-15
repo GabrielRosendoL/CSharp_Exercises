5058c91bcdaa95db3455f67e1dbc52d643f906f0
Gabriel Rosendo <gabrielrosendoleitesilva@gmail.com>
2025-04-15 08:27:57 -0300

    Update New Ex04

---
+++ Ex04.cs
@@ -0,0 +1,38 @@
+// Exercício 04: Calculando o IMC (Índice de Massa Corporal)
+// 
+// Objetivo: Completar o código abaixo para calcular o IMC de uma pessoa.
+// 
+// Instruções:
+// 1. Solicite ao usuário que digite seu peso em quilogramas
+// 2. Solicite ao usuário que digite sua altura em metros
+// 3. Leia as entradas do usuário
+// 4. Converta as entradas para números decimais (double)
+// 5. Calcule o IMC usando a fórmula: IMC = peso / (altura * altura)
+// 6. Exiba o resultado no console com duas casas decimais
+// 
+// Dica: Use double.Parse() para converter strings em números decimais
+// Dica: Use Math.Round() para arredondar o resultado
+// Dica: Use string.Format() ou $"" para formatar o número com casas decimais
+// Dica: A altura deve ser em metros (ex: 1.75 para 1 metro e 75 centímetros)
+
+class Ex04
+{
+    public static void Run()
+    {
+        // TODO: Solicite o peso do usuário
+        
+        // TODO: Leia o peso
+        
+        // TODO: Solicite a altura do usuário
+        
+        // TODO: Leia a altura
+        
+        // TODO: Converta as entradas para números decimais
+        
+        // TODO: Calcule o IMC
+        // IMC = peso / (altura * altura)
+        
+        // TODO: Exiba o resultado formatado com duas casas decimais
+        
+    }
+} 
